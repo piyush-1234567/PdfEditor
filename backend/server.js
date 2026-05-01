07d24455a79a2ec3866be89af3b0cc39c407757c
Piyush Jain <piyushjain07032004@gmail.com>
2026-05-01 21:56:31 +0530

    make the project_journal to understand the project life

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,17 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
+const express = require("express");  // import expressjs library
+const cors = require("cors"); // import cors 
+const app = express(); // create your server instance 
+
+// allows frontend to backend
+app.use(cors());
+// allows json data handling
+app.use(express.json());
+// health check if server is running or not
+app.get("/",(req,res) =>{
+    res.send("Server is Running ...");
+});
+
+const PORT = 5000;
+app.listen(PORT, (req,res)=>{
+    console.log(`Server is running on port : ${PORT}`);
+});
