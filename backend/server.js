c2d7121ed60fdbc79296db8baac6fbcca1fc41b0
Piyush Jain <piyushjain07032004@gmail.com>
2026-05-02 23:41:11 +0530

    uses the multer library

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,33 @@
 const express = require("express");  // import expressjs library
 const cors = require("cors"); // import cors 
 const app = express(); // create your server instance 
-
+const multer = require("multer"); // multer is a nodejs middleware for handling multipart/form-data which is primarily used for uploading files 
 // allows frontend to backend
 app.use(cors());
 // allows json data handling
 app.use(express.json());
 // health check if server is running or not
+const storage = multer.diskStorage({
+    destination:(req,file,cb) =>{
+        cb(null, "uploads/");
+    },
+    filename:(req, file, cb) =>{
+        cb(null, Date.now() + "-" + originalname);
+    }
+});
+
+
+const upload = multer({storage});   
+app.post("/uploade", upload.single("pdf"), (req,res)=>{
+    res.json({
+        message: "File uploaded successfully",
+        filepath: req.file.path
+    });
+});
+
+app.use("/files",express.static("uploads"));
+
+
 app.get("/",(req,res) =>{
     res.send("Server is Running ...");
 });
