7e32346bb1c9a9a2c1fe959a662193f4ce0951aa
Piyush Jain <piyushjain07032004@gmail.com>
2026-05-07 23:21:37 +0530

    clear a bug

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const storage = multer.diskStorage({
         cb(null, "uploads/");
     },
     filename:(req, file, cb) =>{
-        cb(null, Date.now() + "-" + originalname);
+        cb(null, Date.now() + "-" + file.originalname);
     }
 });
 
