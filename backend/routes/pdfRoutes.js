4c7fb568a6379c814e3ea82ebd3c8a186c312929
Piyush Jain <145677033+piyush-1234567@users.noreply.github.com>
2026-05-18 22:04:32 +0530

    just made the logic for pdfRoutes

--- backend/routes/pdfRoutes.js
+++ backend/routes/pdfRoutes.js
@@ -2,3 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { UploadPdf } = require("controllers/pdfController");
 
+const upload = require("../middleware/uploadMiddleware");
+
+router.post("/upload",upload.single("pdf"),UploadPdf);
+
+module.exports = router;
