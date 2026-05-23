c38302190b43f115c40426658956d000a1265526
Piyush Jain <145677033+piyush-1234567@users.noreply.github.com>
2026-05-23 23:38:25 +0530

    added react useState hooks to store the pdf in react object

--- Frontend/src/components/UploadPdf.jsx
+++ Frontend/src/components/UploadPdf.jsx
@@ -1,6 +1,10 @@
+import {useState} from 'react';
 export default function UploadPdf(){
+    const [pdfFile, setPdfFile] = useState(null);
+    // grab the first file user selected and console it to the browser
     const handleFile = (e)=>{
         const file = e.target.files[0];
+        setPdfFile(file);
         console.log(file);
     }
     return (
