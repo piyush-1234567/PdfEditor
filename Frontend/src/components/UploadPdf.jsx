5c8e4a876ad2520b7c4b7b7d5c08bc352a44e538
Piyush Jain <145677033+piyush-1234567@users.noreply.github.com>
2026-05-23 22:48:16 +0530

    react getting the pdfObject from frontend

--- Frontend/src/components/UploadPdf.jsx
+++ Frontend/src/components/UploadPdf.jsx
@@ -1,11 +1,19 @@
 export default function UploadPdf(){
+    const handleFile = (e)=>{
+        const file = e.target.files[0];
+        console.log(file);
+    }
     return (
         
         <div>
             <label htmlFor="pdfUpload">
                 Upload PDF
             </label>
-            <input type='file' accept=".pdf"/>
+            <input 
+                type='file' 
+                accept=".pdf" 
+                onChange={handleFile}
+            />
         </div>
     )
 }
