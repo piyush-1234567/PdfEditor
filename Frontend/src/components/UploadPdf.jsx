87d20e2e90da5e35d4462bc6a5dd4a9a91609705
Piyush Jain <145677033+piyush-1234567@users.noreply.github.com>
2026-05-24 23:42:49 +0530

    just converted the pdf file to buffer

--- Frontend/src/components/UploadPdf.jsx
+++ Frontend/src/components/UploadPdf.jsx
@@ -2,9 +2,10 @@ import {useState} from 'react';
 export default function UploadPdf(){
     const [pdfFile, setPdfFile] = useState(null);
     // grab the first file user selected and console it to the browser
-    const handleFile = (e)=>{
+    const handleFile = async (e)=>{
         const file = e.target.files[0];
         setPdfFile(file);
+        const arrayBuffer = await file.arrayBuffer();
         console.log(file);
     }
     return (
