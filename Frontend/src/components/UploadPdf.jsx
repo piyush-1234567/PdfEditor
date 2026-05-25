7fac8817596825443acf5b9b2d4b2158e3bf726f
Piyush Jain <145677033+piyush-1234567@users.noreply.github.com>
2026-05-25 22:32:09 +0530

    just rendered the page on frontend

--- Frontend/src/components/UploadPdf.jsx
+++ Frontend/src/components/UploadPdf.jsx
@@ -1,10 +1,11 @@
-import {useState} from 'react';
+import { useState, useRef } from 'react';
 import * as pdfjsLib from 'pdfjs-dist';
-pdfjsLib.GlobalWorkerOptions.workerSrc = 
-  `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.js`;
+import pdfWorker from 'pdfjs-dist/build/pdf.worker?url';
 
+pdfjsLib.GlobalWorkerOptions.workerSrc = pdfWorker;
 export default function UploadPdf(){
     const [pdfFile, setPdfFile] = useState(null);
+    const canvasRef = useRef(null);
     // grab the first file user selected and console it to the browser
     const handleFile = async (e)=>{
         const file = e.target.files[0];
@@ -12,6 +13,30 @@ export default function UploadPdf(){
         const arrayBuffer = await file.arrayBuffer();
         const loadingTask = pdfjsLib.getDocument({data: arrayBuffer});
         const pdfDoc = await loadingTask.promise;
+
+        // get first page
+        const page = await pdfDoc.getPage(1);
+
+        const scale = 1.5;
+        const viewport = page.getViewport({ scale });
+
+
+        // get canvas
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+
+
+         // set canvas size
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+         // render pdf page
+        await page.render({
+            canvasContext: context,
+            viewport: viewport,
+        }).promise;
+
+        console.log("PDF Rendered");
         console.log(file);
     }
     return (
@@ -25,6 +50,10 @@ export default function UploadPdf(){
                 accept=".pdf" 
                 onChange={handleFile}
             />
+            <canvas ref={canvasRef}
+            style={{border: "1px solid black"}}>
+                
+            </canvas>
         </div>
     )
 }
